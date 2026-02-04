8edb17d707c9fcec36a5efa841cd961df4030ddb
PrachiJadhav9890 <prachij2432@gmail.com>
2026-02-04 17:33:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,3 +46,7 @@ VALUES ('{ingredients_string}', '{name_on_order}')
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
+      
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
